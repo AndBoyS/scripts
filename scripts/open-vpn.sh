f066e8f0ff4f9b9fdd02cdd294a428d8c746f7bc
AndBoyS <andboyxd@gmail.com>
2024-08-11 12:22:39 +0300

    open vpn, warp switch

---
+++ scripts/open-vpn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open VPN
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author andboyxd
+# @raycast.authorURL https://raycast.com/andboyxd
+
+killall Transmission
+open -a "V2BOX"
---
+++ scripts/warp-turn-on.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Warp turn on
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author andboyxd
+# @raycast.authorURL https://raycast.com/andboyxd
+
+warp-cli connect
+
